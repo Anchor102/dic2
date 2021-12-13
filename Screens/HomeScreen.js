3eb5e967c3f3352084bb6f09f1d7402602f82705
Kunwarveer Singh <thekunwarveer@gmail.com>
2021-12-13 20:26:26 +0530

    dictionary

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,47 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Touchable } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Touchable } from 'react-native';
+import * as Speech from 'expo-speech';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export default class HomeScreen extends React.Component{
 
   constructor(){
     super()
-    this.state = {
-      word1: "",
-      text1: "",
-      definition: ""
+    this.state = { 
+      word: '', 
+      definition: '', 
+      text:'', 
     }
   }
 
-  getWord = async (text1) =>{
-    var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text1
-    return fetch(url).then((data)=>{return data.json})
-    .then((response)=>{var text1 = response[0].text1;
+  getWord = async (word) =>{
+    var url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    const data = await fetch(url)
+    const response = await data.json()
+    var word_1 = response[0].word
     var definition = response[0].meanings[0].definitions[0].definition
+    
     this.setState({
-      text1: text1.trim(),
+      word: word_1.trim(),
       definition: definition.trim()
-    })
     })
   }
 
   render(){
     return (
-      <View style={styles.container}>
-        <View style = {styles.boxContainer}>
-          <TextInput placeholder = "Enter The Word Here" onChangeText ={(input)=>{
-            this.setState({word1: input})
-          }}  
-          style = {styles.inputBox}
-          />
+      <View style={styles.container}> 
+      <Image style = {styles.dictionaryImage}
+          source={require("../assets/bg.png")}/>
+        <View style = {styles.boxContainer}> 
+          <TextInput 
+            placeholder = "Enter The Word Here" 
+            onChangeText ={(text)=>{ this.setState({text: text, }) }} 
+            style = {styles.inputBox} 
+          />  
           <TouchableOpacity 
           style = {styles.button} 
-          onPress  = {()=>{this.setState({text1: this.state.word1})
-          this.getWord(this.state.text1)}} >
+          onPress  = {()=>{this.getWord(this.state.text)}} >
             <Text style = {styles.searchText}> Search </Text>
           </TouchableOpacity>
         </View>
-        <Text>{this.state.text1}</Text>
         <Text>{this.state.definition}</Text>
+
+        <View style = {styles.iconContainer}>
+        <TouchableOpacity
+          onPress = {()=>{ Speech.speak(this.state.definition) }} 
+        > 
+        <Ionicons 
+          name={this.state.speakerIcon} 
+          size={RFValue(30)} 
+          color= "black"
+          style={{ margin: RFValue(15) }} 
+        />
+        </TouchableOpacity>
+        </View>
+        
+
       </View>
     );
   }
@@ -56,16 +75,20 @@ const styles = StyleSheet.create({
   inputBox: {
     textAlign: 'center',
     alignSelf: 'center',
-    width: "80%",
+    width: "120%",
     height: 40,
     borderWidth: 5,
+    color:"black",
+    borderColor: "#3373BA",
+    marginTop: 10
     
   },
   button: {
-    width: 40,
-    height: 40,
+    width: 60,
+    height: 30,
     justifyContent: 'center',
     alignItems: 'center',
+    alignContent: "center",
     borderWidth: 2,
     borderRadius: 10,
     margin: 10
@@ -74,10 +97,19 @@ const styles = StyleSheet.create({
   boxContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: "row"
+    
   },
    searchText: {
      fontSize: 12,
      fontWeight: "bold"
+   },
+   dictionaryImage: {
+     width: 200,
+     height: 200,
+     marginTop: 20,
+   },
+   iconContainer:{
+     flex: 0.2,
+    
    }
 });
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAl4r9M99mwpLzhvfbgZV0BTLSYj-ICkuE",
+    authDomain: "dictionary-38354.firebaseapp.com",
+    projectId: "dictionary-38354",
+    storageBucket: "dictionary-38354.appspot.com",
+    messagingSenderId: "452223229234",
+    appId: "1:452223229234:web:cc3f8bd465d76ee9bc4b2b"
+  };
+  
